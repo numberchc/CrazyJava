799b0a39053099da16808caf045ef76b84a1f470
numberchc <529543728@qq.com>
2015-06-14 22:50:46 +0800

    study AWT control

--- src/com/chc/gui/main/GUIMain.java
+++ src/com/chc/gui/main/GUIMain.java
@@ -1,10 +1,34 @@
 package com.chc.gui.main;
 
+import java.awt.Button;
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.ScrollPane;
+import java.awt.TextField;
+
 public class GUIMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Frame f = new Frame("测试窗口");
+		
+//		Panel p = new Panel();
+//		p.add(new TextField(20));
+//		p.add(new Button("p单击我"));
+//		f.add(p);
+		
+//		ScrollPane sp = new ScrollPane(ScrollPane.SCROLLBARS_ALWAYS);
+//		sp.add(new TextField(20));
+//		sp.add(new Button("sp单击我"));
+//		f.add(sp);
+		
+		f.setLayout(new FlowLayout(FlowLayout.LEFT, 20, 5));
+		for (int i = 0; i < 100; i++) {
+			f.add(new Button("按钮"+i));
+		}
+		//设置窗口为最佳大小
+		f.pack();
+		f.setVisible(true);
 	}
 
 }
