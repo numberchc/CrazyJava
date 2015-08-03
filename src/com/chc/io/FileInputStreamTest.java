8cce8a697ad8ff8dda7340eb74b6f7e364ac4127
chenhuichang <529543728@qq.com>
2015-08-03 23:59:03 +0800

    IO

--- src/com/chc/io/FileInputStreamTest.java
+++ src/com/chc/io/FileInputStreamTest.java
@@ -17,7 +17,7 @@ public class FileInputStreamTest
 	{
 		// 创建字节输入流
 		FileInputStream fis = new FileInputStream(
-			"src/com/chc/io/FileInputStreamTest.java");
+			"./src/com/chc/io/FileInputStreamTest.java");
 		// 创建一个长度为1024的“竹筒”
 		byte[] bbuf = new byte[1024];
 		// 用于保存实际读取的字节数
